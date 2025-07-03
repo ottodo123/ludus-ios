f380db09d68806a381165ceaaa1ef530ac5fe8a4
ottodo123 <otto@blind.com>
2025-07-03 18:14:06 -0400

    Initial Commit

---
+++ Ludus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Ludus
+//
+//  Created by Otto Do on 7/3/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LudusTests/LudusTests.swift
@@ -0,0 +1,16 @@
+//
+//  LudusTests.swift
+//  LudusTests
+//
+//  Created by Otto Do on 7/3/25.
+//
+
+import Testing
+
+struct LudusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
